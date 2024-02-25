f02e945c0de8b080adec3d2676dbc45cd03eab9e
rutiksanap44 <99959986+rutiksanap44@users.noreply.github.com>
2024-02-25 01:31:06 -0500

    Get Post and Get by id API created

--- server/config.js
+++ server/config.js
@@ -1,1 +1,3 @@
 export const PORT = 3333;
+
+export const mongoDBURL = 'mongodb+srv://bookstore:bookstore@cluster0.crhjjnj.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Cluster0'
--- server/index.js
+++ server/index.js
@@ -1,8 +1,80 @@
 import express from "express";
-import { PORT } from "./config.js";
+import { PORT,mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
+import { Book } from "./model/bookModel.js";
 
 const app = express();
 
-app.listen(PORT , () => {
-    console.log(`App is working on PORT NO : ${PORT}`)
+app.use(express.json())
+
+app.post('/books',async(request, response)=> {
+     try{
+        if(
+            !request.body.title ||
+            !request.body.author ||
+            !request.body.publishYear
+        ){
+            return response.status(400).send({
+                message : 'Send all the required fields : Title, Author and Publish Year'
+            })
+        }
+        const newBook = {
+            title : request.body.title,
+            author : request.body.author,
+            publishYear : request.body.publishYear,
+        }
+        const book = await Book.create(newBook)
+        return response.status(500).send(book)
+    }
+    catch(error){
+        console.log(error.message)
+        response.status(500).send({message : error.message})
+    }
+})
+
+app.get('/books', async(request,response)=> {
+    try{
+        const books = await Book.find({})
+        return response.status(200).json({
+            count : books.length,
+            data : books
+        })
+    }
+    catch(error){
+        console.log(error.message)
+        response.send(400).send({message:error.message})
+    }
+})
+
+app.get('/books/:id', async(request,response)=> {
+    try{
+        const {id} = request.params
+        const book = await Book.findById(id)
+        return response.status(200).json({
+            book
+        })
+    }
+    catch(error){
+        console.log(error.message)
+        response.send(400).send({message:error.message})
+    }
+})
+
+
+app.get('/',(request,response)=> {
+    console.log(request)
+    return response.status(200).send('welcome to Rutik\'s Books Store')
+})
+
+
+mongoose
+.connect(mongoDBURL)
+.then(() => {
+    console.log('App is Connected with the Book Store Database')
+    app.listen(PORT , () => {
+        console.log(`App is working on PORT NO : ${PORT}`)
+    })
+})
+.catch((error) => {
+    console.log(error)
 })
